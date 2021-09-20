1a4171c53928b21b482db8524297f3412bbe8d7f
Goodluck <goodluckndumanya1@gmail.com>
2021-09-20 09:09:32 +0100

    Clane api scaffold

--- src/main/java/com/clane/app/security/user/UserController.java
+++ src/main/java/com/clane/app/security/user/UserController.java
@@ -3,7 +3,10 @@ package com.clane.app.security.user;
 import com.clane.app.core.data.ClaneResponse;
 import com.clane.app.core.data.ResponseMessages;
 import com.clane.app.security.user.dto.CreateUserDto;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@Api(value="Api for creating and managing users")
 public class UserController {
 
     private final UserService userService;
@@ -21,7 +25,12 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    @ApiOperation("For creating a new user")
+    @ApiOperation(value = "For creating a new user", response = ClaneResponse.class, consumes = "application/json", produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = ResponseMessages.USER_CREATED),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to access is not found")
+    })
     public ResponseEntity<ClaneResponse> createUser(@Valid @RequestBody CreateUserDto userDto) {
         return ResponseEntity.ok(ClaneResponse.builder()
                 .message(ResponseMessages.USER_CREATED)
@@ -29,11 +38,12 @@ public class UserController {
                 .payload(userService.newUser(userDto)).build());
     }
 
-    @GetMapping("/findUserByPhone/{phone}")
-    public ResponseEntity<ClaneResponse> findUserByPhone(@PathVariable String phone) {
+    @GetMapping("/findUserByPhoneOrEmail/{ref}")
+    @ApiOperation(value = "For testing that the cache works", response = ClaneResponse.class, consumes = "string", produces = "application/json")
+    public ResponseEntity<ClaneResponse> findUserByPhone(@PathVariable String ref) {
         return ResponseEntity.ok(ClaneResponse.builder()
                 .message("")
                 .status(HttpStatus.OK.value())
-                .payload(userService.findByEmailOrPhoneNumber(phone)).build());
+                .payload(userService.findByEmailOrPhoneNumber(ref)).build());
     }
 }
--- src/main/java/com/clane/app/wallet/WalletController.java
+++ src/main/java/com/clane/app/wallet/WalletController.java
@@ -5,7 +5,10 @@ import com.clane.app.core.data.ResponseMessages;
 import com.clane.app.wallet.dto.TopUp;
 import com.clane.app.wallet.dto.Transfer;
 import com.clane.app.wallet.transaction.Transaction;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,7 @@ import java.text.MessageFormat;
 
 @RestController
 @RequestMapping("wallets")
+@Api(value="Api for carrying out wallet related activities")
 public class WalletController {
 
     private final WalletService walletService;
@@ -27,20 +31,29 @@ public class WalletController {
     }
 
     @PostMapping("/topUp")
-    @ApiOperation("For manual top-up of a user wallet")
+    @ApiOperation(value = "For manual top up of a clane user wallet", response = ClaneResponse.class, consumes = "application/json", produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = ResponseMessages.TOP_UP_SUCCESS),
+    })
     public ResponseEntity<ClaneResponse> walletTopUp(@Valid @RequestBody TopUp topUp) {
         return ResponseEntity.ok(ClaneResponse.builder()
                 .message(ResponseMessages.TOP_UP_SUCCESS)
                 .status(HttpStatus.CREATED.value())
-                .payload(walletService.topUp(topUp)).build());
+                .payload(walletService.topUp(topUp))
+                .build());
     }
 
     @PostMapping("/transfer")
+    @ApiOperation(value = "For transfer between two registered clane user wallets", response = ClaneResponse.class, consumes = "application/json", produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = ResponseMessages.TRANSFER_SUCCESS),
+    })
     public ResponseEntity<ClaneResponse> transfer(@Valid @RequestBody Transfer transfer) {
         Transaction transaction = walletService.transfer(transfer);
         return ResponseEntity.ok(ClaneResponse.builder()
                 .message(MessageFormat.format(ResponseMessages.TRANSFER_SUCCESS, transaction.getReceiver()))
                 .status(HttpStatus.CREATED.value())
-                .payload(transaction).build());
+                .payload(transaction)
+                .build());
     }
 }
